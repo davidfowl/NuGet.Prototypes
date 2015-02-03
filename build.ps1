5bc55d0ac17796bbde0a45fcdbaf1348dc5a90a2
David Fowler <davidfowl@gmail.com>
2015-02-03 11:38:29 -0800

    Remove arfiacts before building

--- build.ps1
+++ build.ps1
@@ -1,11 +1,12 @@
 build\kvm.ps1 upgrade
-kpm restore
 $scriptPath = split-path $MyInvocation.MyCommand.Path -parent
 $artifacts = Join-Path $scriptPath "artifacts"
 $nuget3Output = Join-Path $artifacts "nuget3"
 $packageOutput = Join-Path $artifacts "packages"
 $ts = [DateTime]::UtcNow.Ticks.ToString()
 $env:K_BUILD_VERSION="t$ts"
+rm -r -force $artifacts
 mkdir $packageOutput -Force | Out-Null
+kpm restore
 ls -r project.json | ?{ $_.Directory.Name -ne "NuGet3" } | %{ kpm pack $_; cp (Join-Path (Join-Path $_.Directory "bin\Debug") "$($_.Directory.Name).1.0.0-t$ts.nupkg") $packageOutput }
 kpm bundle --no-source src\NuGet3 --out $nuget3Output
