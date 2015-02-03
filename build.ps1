e3bfad8992b6e37162c1e5454a251363a5ef72d9
David Fowler <davidfowl@gmail.com>
2015-02-02 23:36:54 -0800

    Make the net45 facades build time only

--- build.ps1
+++ build.ps1
@@ -5,6 +5,6 @@ $nuget3Output = Join-Path $artifacts "nuget3"
 $packageOutput = Join-Path $artifacts "packages"
 $ts = [DateTime]::UtcNow.Ticks.ToString()
 $env:K_BUILD_VERSION="t$ts"
-mkdir $artifacts -Force | Out-Null
+mkdir $packageOutput -Force | Out-Null
 ls -r project.json | ?{ $_.Directory.Name -ne "NuGet3" } | %{ kpm pack $_; cp (Join-Path (Join-Path $_.Directory "bin\Debug") "$($_.Directory.Name).1.0.0-t$ts.nupkg") $packageOutput }
 kpm bundle --no-source src\NuGet3 --out $nuget3Output
